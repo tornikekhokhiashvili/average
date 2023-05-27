cd30567929023fc4d1898eafe4d9b01e733e40ee
tornikee <tornikexoxiashvili@gmail.com>
2023-05-27 14:07:08 +0400

    change

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -5,7 +5,21 @@ import java.util.Scanner;
 public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Use Scanner methods to read input
+        int count=0;
+        int total=0;
+        int[] numbers=new int[100];
+
+        for (int i=0;i<100;i++) {
+            int num=scanner.nextInt();
+            if (num==0){
+                break;
+            }
+            numbers[i] = num;
+            count++;
+            total = total + num;
+        }
+        int average=total/count;
+        System.out.println(average);
 
     }
 
